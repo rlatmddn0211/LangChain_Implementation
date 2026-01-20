3ecce6b2972450aa5aedbdcd33c6f417b598fa63
rlatmddn0211 <157192341+rlatmddn0211@users.noreply.github.com>
2026-01-20 17:39:24 +0900

    feat: Streamlit from Scratch, virtual Chatbot page

---
+++ home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+st.set_page_config(
+    page_title="FullStackGPT Home",
+    page_icon="📑",
+)
+st.title("FullStackGPT Home")
+st.markdown(
+    """
+# Hello!
+
+Welcome to my FullStackGPT Protfolio!
+
+Here are the apps I made:
+- [ ] [DocumentGPT](/DocumentGPT)
+- [ ] [PrivateGPT](/PrivateGPT)
+- [ ] [QuizGPT](/QuizGPT)
+- [ ] [SiteGPT](/SiteGPT)
+- [ ] [MeetingGPT](/MeetingGPT)
+- [ ] [InverstorGPT](/InverstorGPT)
+    """
+)
